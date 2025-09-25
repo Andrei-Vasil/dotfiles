bca737338e2cd960648f9be93146258ec43036c3
Andrei Vasil <andrei.vasil@n26.com>
2025-09-25 12:20:22 +0200

    search across hidden files

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,56 +1,66 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-			local actions = require("telescope.actions")
-			require("telescope").setup({
-				defaults = {
-					mappings = {
-						i = {
-							["<C-c>"] = false, -- similar behavior to vim motions
-							["<esc>"] = actions.close,
-						},
-					},
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-	},
-	{
-		"ANGkeith/telescope-terraform-doc.nvim",
-		dependencies = {
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("terraform_doc")
-		end,
-	},
-	{
-		"cappyzawa/telescope-terraform.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-telescope/telescope.nvim",
-		},
-		config = function()
-			require("telescope").load_extension("terraform")
-		end,
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    config = function()
+      local builtin = require("telescope.builtin")
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      local actions = require("telescope.actions")
+      require("telescope").setup({
+        defaults = {
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+          },
+          mappings = {
+            i = {
+              ["<C-c>"] = false, -- similar behavior to vim motions
+              ["<esc>"] = actions.close,
+            },
+          },
+        },
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+  },
+  {
+    "ANGkeith/telescope-terraform-doc.nvim",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("terraform_doc")
+    end,
+  },
+  {
+    "cappyzawa/telescope-terraform.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    config = function()
+      require("telescope").load_extension("terraform")
+    end,
+  },
 }
